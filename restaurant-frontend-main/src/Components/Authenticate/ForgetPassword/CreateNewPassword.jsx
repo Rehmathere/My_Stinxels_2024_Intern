969bd624095877d78a04c7ef005149e06464fd04
REHMAT QAZI <103884066+Rehmathere@users.noreply.github.com>
2024-11-08 18:09:47 +0500

    Add files via upload

--- restaurant-frontend-main/src/Components/Authenticate/ForgetPassword/CreateNewPassword.jsx
+++ restaurant-frontend-main/src/Components/Authenticate/ForgetPassword/CreateNewPassword.jsx
@@ -4,6 +4,11 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createNewPasswordThunk } from "../../../Redux/Thunks/UserApi";
 import { Form, Input, Button } from "antd";
+// CSS
+import "./ForgetPass.scss";
+// Image
+import ForgetPass from "../../../assets/NewPass.png";
+
 
 function CreateNewPassword() {
   const dispatch = useDispatch();
@@ -26,7 +31,8 @@ function CreateNewPassword() {
 
   return (
     <>
-      <div className="h-[100vh] w-full flex justify-center items-center bg-stone-200">
+      {/* --- Old Code --- */}
+      {/* <div className="h-[100vh] w-full flex justify-center items-center bg-stone-200">
         <div className="bg-blue w-[400px] h-[300px] bg-white">
           <div className="flex flex-col justify-center items-center p-4 gap-3 h-full">
             <h1 className="text-2xl font-bold">Create New Password</h1>
@@ -72,6 +78,70 @@ function CreateNewPassword() {
                 </Button>
               </Form.Item>
             </Form>
+          </div>
+        </div>
+      </div> */}
+      {/* --- New Code --- */}
+      <div className="Parent_ForgetPass_Whole">
+        <div className="Parent_ForgetPass_Whole_Sub">
+          {/* Box */}
+          <div className="ForgetPass_Box">
+            {/* Part 1 */}
+            <div className="Forget_Img">
+              <img src={ForgetPass} alt="NA" />
+            </div>
+            {/* Part 2 */}
+            <div className="Forget_Heading">
+              <h1>Create New Password</h1>
+              <p>Please Create New Password</p>
+            </div>
+            {/* Part 3 */}
+            <div className="Forget_MyForm">
+              <Form style={{ width: "100%" }} onFinish={handleFinish}>
+                <Form.Item
+                  name={"newPassword"}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Password",
+                    },
+                    {
+                      pattern: new RegExp(/^(?=.*[A-Z]).*$/),
+                      message: "At least one Uppercase letter",
+                    },
+                    {
+                      pattern: new RegExp(/^(?=.*[a-z]).*$/),
+                      message: "At least one Lowercase letter",
+                    },
+                    {
+                      pattern: new RegExp(/^(?=.*[0-9]).*$/),
+                      message: "At least one number",
+                    },
+                    {
+                      pattern: new RegExp(
+                        /^(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?`~\-]).*$/
+                      ),
+                      message: "At least one special character",
+                    },
+                    {
+                      pattern: new RegExp(/^.{8,}$/),
+                      message: "Password must be at least 8 characters long",
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    className="MyInput"
+                    placeholder="Enter Password"
+                  ></Input.Password>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button type="primary" block htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
--- restaurant-frontend-main/src/Components/Authenticate/ForgetPassword/EnterOtp.jsx
+++ restaurant-frontend-main/src/Components/Authenticate/ForgetPassword/EnterOtp.jsx
@@ -6,6 +6,11 @@ import {
   forgetPasswordThunk,
 } from "../../../Redux/Thunks/UserApi";
 import { useNavigate } from "react-router-dom";
+// CSS
+import "./ForgetPass.scss";
+// Images
+import OTP from "../../../assets/OTP.png";
+
 function EnterOtp() {
   const verified_email = localStorage.getItem("verified_email");
   const navigate = useNavigate();
@@ -26,7 +31,8 @@ function EnterOtp() {
 
   return (
     <>
-      <div className="h-[100vh] w-full flex justify-center items-center bg-stone-200">
+      {/* --- Old Code --- */}
+      {/* <div className="h-[100vh] w-full flex justify-center items-center bg-stone-200">
         <div className="bg-blue w-[400px] h-[300px] bg-white">
           <div className="flex flex-col justify-center items-center p-4 gap-3 h-full">
             <h1 className="text-2xl font-bold">Enter OTP</h1>
@@ -51,6 +57,47 @@ function EnterOtp() {
             >
               Resend OTP
             </Button>
+          </div>
+        </div>
+      </div> */}
+      {/* --- New Code --- */}
+      <div className="Parent_ForgetPass_Whole">
+        <div className="Parent_ForgetPass_Whole_Sub">
+          {/* Box */}
+          <div className="ForgetPass_Box">
+            {/* Part 1 */}
+            <div className="Forget_Img">
+              <img src={OTP} alt="NA" />
+            </div>
+            {/* Part 2 */}
+            <div className="Forget_Heading">
+              <h1>Enter OTP</h1>
+              <p>An OTP has been sent to {email}</p>
+            </div>
+            {/* Part 3 */}
+            <div className="Forget_MyForm">
+              <Form style={{ width: "100%" }}>
+                <Form.Item>
+                  <Input.OTP
+                    length={4}
+                    style={{ width: "100%" }}
+                    formatter={(str) => str.toUpperCase()}
+                    {...sharedProps}
+                  />
+                </Form.Item>
+              </Form>
+              <Button
+                type="link"
+                onClick={() =>
+                  dispatch(
+                    forgetPasswordThunk({ email: verified_email, navigate })
+                  )
+                }
+                className="My_Back_Btn"
+              >
+                Resend OTP
+              </Button>
+            </div>
           </div>
         </div>
       </div>
